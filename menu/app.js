94abbb746db9dbb9bdc4bb6a900bd8c7ca39cc24
MOHI-UDDIN-AKBAR <mohiuddinakbar397720@gmail.com>
2025-10-22 00:03:28 +0200

    feat: Add menu display with category filtering and styling

---
+++ menu/app.js
@@ -0,0 +1,62 @@
+import menu from "./basic.js";
+
+// DOM Elements
+const menuItemsContainer = document.querySelector(".menu__items");
+const categoryButtons = document.querySelectorAll(".menu__category-btn");
+
+// Render Menu Items
+function renderMenuItems(menuData = []) {
+  if (!menuData && !menuData.length) return;
+
+  const html = menuData
+    .map((item) => {
+      const { title, price, img, desc } = item;
+      return `<article class="menu-item">
+        <figure class="menu-item__figure">
+        <img
+        src=${img}
+        alt=${title}
+        loading="lazy"
+        class="menu-item__image"
+        />
+        <figcaption class="menu-item__details">
+        <header class="menu-item__header">
+        <h3 class="menu-item__name">${title}</h3>
+        <span class="menu-item__price">$${price}</span>
+        </header>
+        <p class="menu-item__description">
+        ${desc}
+        </p>
+        </figcaption>
+        </figure>
+        </article>`;
+    })
+    .join("");
+  menuItemsContainer.innerHTML =
+    html || `<p class="menu__empty">No Items Found</p>`;
+}
+
+// Filter Menu Items By Category
+function filterMenuItemsByCategory(category) {
+  if (category === "all") {
+    return menu;
+  } else {
+    return menu.filter(
+      (menuItem) => menuItem.category === category.toLowerCase()
+    );
+  }
+}
+
+// Event Listener
+
+categoryButtons.forEach((category) => {
+  category.addEventListener("click", () => {
+    const selectedCategory = category.textContent.trim().toLowerCase();
+    const filteredMenuItems = filterMenuItemsByCategory(selectedCategory);
+    renderMenuItems(filteredMenuItems);
+  });
+});
+
+//  Initial Render
+
+document.addEventListener("DOMContentLoaded", () => renderMenuItems(menu));
